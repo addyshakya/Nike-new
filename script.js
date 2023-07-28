78f0797c5b98decc42f223af6e1443bc2fc15bd9
aditya <shakyaaditya469@gmail.com>
2023-07-28 09:58:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+document.querySelector('#Men').addEventListener('click', function(){
+    document.querySelector("#main").style.display = "none",
+    document.querySelector("#main2").style.display = "block"
+    document.querySelector("#main3").style.display = "none"
+})
+
+document.querySelector('#mens').addEventListener('click', function(){
+    document.querySelector("#main").style.display = "none",
+    document.querySelector("#main2").style.display = "block"
+    document.querySelector("#main3").style.display = "none"
+})
+
+document.querySelector('#women').addEventListener('click', function(){
+    document.querySelector("#main").style.display = "none",
+    document.querySelector("#main2").style.display = "none",
+    document.querySelector("#main3").style.display = "block"
+
+})
+
+document.querySelector('#womens').addEventListener('click', function(){
+    document.querySelector("#main").style.display = "none",
+    document.querySelector("#main2").style.display = "none",
+    document.querySelector("#main3").style.display = "block"
+})
+
+document.querySelector('#wo').addEventListener('click', function(){
+    document.querySelector("#main").style.display = "none",
+    document.querySelector("#main2").style.display = "none",
+    document.querySelector("#main3").style.display = "block"
+
+})
+
+document.querySelector('#me').addEventListener('click', function(){
+    document.querySelector("#main").style.display = "none",
+    document.querySelector("#main2").style.display = "block"
+    document.querySelector("#main3").style.display = "none"
+})
+
+
+document.querySelector('#wom').addEventListener('click', function(){
+    document.querySelector("#main").style.display = "none",
+    document.querySelector("#main2").style.display = "none",
+    document.querySelector("#main3").style.display = "block"
+
+})
+
+document.querySelector('#men').addEventListener('click', function(){
+    document.querySelector("#main").style.display = "none",
+    document.querySelector("#main2").style.display = "block"
+    document.querySelector("#main3").style.display = "none"
+})
+
+
